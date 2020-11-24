8ae5a54cd0efb5fe9397fdfd36b26dce9927ca47
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-24 16:53:04 +0200

    Brew formula update for route53-cli version v0.2.1

--- r53.rb
+++ r53.rb
@@ -2,17 +2,20 @@
 class R53 < Formula
   desc "Cli tool for Route53 to quickly query values of records"
   homepage "https://github.com/Isan-Rivkin/route53-cli"
-  version "0.2.0"
+  version "0.2.1"
   bottle :unneeded
+
   if OS.mac?
-    url "https://github.com/Isan-Rivkin/route53-cli/releases/download/v0.2.0/route53-cli_0.2.0_darwin_amd64.tar.gz"
-    sha256 "d479ae63d2c87bed143f578e6e25bb09458711a115fa8298597aeec835026649"
+    url "https://github.com/Isan-Rivkin/route53-cli/releases/download/v0.2.1/route53-cli_0.2.1_darwin_amd64.tar.gz"
+    sha256 "b347fa981b32018b26e6960fbd3a112adc633e63044dbf5d9e5837742ee77a63"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Isan-Rivkin/route53-cli/releases/download/v0.2.0/route53-cli_0.2.0_linux_amd64.tar.gz"
-    sha256 "cf68893fe20e363958568a6bf41497d4729cd494c8926b301b885ea0985390a3"
+    url "https://github.com/Isan-Rivkin/route53-cli/releases/download/v0.2.1/route53-cli_0.2.1_linux_amd64.tar.gz"
+    sha256 "748663d233721929f4a3e42c5b1c8a7dec25a94fcf08f05dd261586d64683501"
   end
+  
   depends_on "git"
+
   def install
     bin.install "r53"
   end
