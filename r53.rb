585e96ef09bb1395aca6034fcac90e64dbca81fe
isan_rivkin <isanrivkin@gmail.com>
2021-12-09 08:46:30 +0200

    Update r53.rb

--- r53.rb
+++ r53.rb
@@ -6,7 +6,6 @@ class R53 < Formula
   desc "Cli tool for Route53 to quickly query values of records"
   homepage "https://github.com/Isan-Rivkin/route53-cli"
   version "0.3.1"
-
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/Isan-Rivkin/route53-cli/releases/download/v0.3.1/route53-cli_0.3.1_darwin_amd64.tar.gz"
@@ -17,12 +16,10 @@ class R53 < Formula
       end
     end
   end
-
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/Isan-Rivkin/route53-cli/releases/download/v0.3.1/route53-cli_0.3.1_linux_arm64.tar.gz"
       sha256 "f89770c187903de6af1c86c948ffcba91130088ead06cb90e5dac9912513d474"
-
       def install
         bin.install "r53"
       end
@@ -30,12 +27,10 @@ class R53 < Formula
     if Hardware::CPU.intel?
       url "https://github.com/Isan-Rivkin/route53-cli/releases/download/v0.3.1/route53-cli_0.3.1_linux_amd64.tar.gz"
       sha256 "321293c2a635bd702a37cf628cad071dba89d4304f393816bbe40e70cc3dac83"
-
       def install
         bin.install "r53"
       end
     end
   end
-
   depends_on "git"
 end
