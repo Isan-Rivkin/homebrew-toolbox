9abe98d52e8f2631732e86ca75809d36f8ccb993
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-04 18:34:50 +0200

    Brew formula update for boltdbweb version v1.0.3

--- boltdbweb.rb
+++ boltdbweb.rb
@@ -5,20 +5,20 @@
 class Boltdbweb < Formula
   desc "Bolt DB Web UI"
   homepage "https://github.com/Isan-Rivkin/boltdbweb"
-  version "1.0.2"
+  version "1.0.3"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/Isan-Rivkin/boltdbweb/releases/download/v1.0.2/boltdbweb_1.0.2_darwin_amd64.tar.gz"
-      sha256 "e24065f2783b4041c8bae8bd29317296b875c1c4367caeb5444e9024520418db"
+    if Hardware::CPU.arm?
+      url "https://github.com/Isan-Rivkin/boltdbweb/releases/download/v1.0.3/boltdbweb_1.0.3_darwin_arm64.tar.gz"
+      sha256 "2e5390ce3d9ef024eb93c60f6fa9b6ceff3138c14dd3d87b0d0b787891c2f745"
 
       def install
         bin.install "boltdbweb"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/Isan-Rivkin/boltdbweb/releases/download/v1.0.2/boltdbweb_1.0.2_darwin_arm64.tar.gz"
-      sha256 "084490c8878ff1499bad09f824ba4ae4b93acdd4a4511f1d1d1e5c5e4dc8d356"
+    if Hardware::CPU.intel?
+      url "https://github.com/Isan-Rivkin/boltdbweb/releases/download/v1.0.3/boltdbweb_1.0.3_darwin_amd64.tar.gz"
+      sha256 "b6fb2539d73f7feddae2293aafe365a042fb271abaa4e3e199b206df45dc747f"
 
       def install
         bin.install "boltdbweb"
@@ -28,16 +28,16 @@ class Boltdbweb < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Isan-Rivkin/boltdbweb/releases/download/v1.0.2/boltdbweb_1.0.2_linux_arm64.tar.gz"
-      sha256 "90a1a3567e0c40113945005bdf539fd458b3b66df5f82b477d321aed03f0b2e1"
+      url "https://github.com/Isan-Rivkin/boltdbweb/releases/download/v1.0.3/boltdbweb_1.0.3_linux_arm64.tar.gz"
+      sha256 "fc789fa4da16d086ce3635e0e1bf7309c6ad5d3ff777c2cb8168632f0f854013"
 
       def install
         bin.install "boltdbweb"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Isan-Rivkin/boltdbweb/releases/download/v1.0.2/boltdbweb_1.0.2_linux_amd64.tar.gz"
-      sha256 "1bcaf24207d84500a22f045f6105100b5d3944578655dbac744217d4e7fa082e"
+      url "https://github.com/Isan-Rivkin/boltdbweb/releases/download/v1.0.3/boltdbweb_1.0.3_linux_amd64.tar.gz"
+      sha256 "643b3decd42cd6077336844ed6740a9eb2dbcd9307270187f225e10ed27f12fd"
 
       def install
         bin.install "boltdbweb"
